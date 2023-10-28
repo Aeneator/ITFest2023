d59fb779d9213c60f021754c0b844551390dda7f
Davidmoise08 <david.moise.08.04@gmail.com>
2023-10-28 16:45:39 +0300

    Server

--- Backend/app.js
+++ Backend/app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const usersRoutes = require("./routes/users-routes");
+const clothesRoutes = require("./routes/clothes-routes");
 const HttpError = require("./models/http-error");
 
 const app = express();
@@ -12,11 +13,9 @@ app.use(cors());
 
 app.use(express.json());
 
-app.use("api/users", usersRoutes);
+app.use("/api/users", usersRoutes);
 
-app.get("/", (req, res, next) => {
-  res.json({ message: "Successful" });
-});
+app.use("/api/clothes", clothesRoutes);
 
 app.use((req, res, next) => {
   const error = new HttpError("Route not found", 404);
