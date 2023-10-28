6fdd2f997e1280dcdcb86387395f8ad44bc239fb
Davidmoise08 <david.moise.08.04@gmail.com>
2023-10-28 17:24:40 +0300

    server

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -6,6 +6,11 @@ const userSchema = new Schema({
     type: String,
     required: true,
   },
+  points: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
   email: {
     type: String,
     unique: true,
